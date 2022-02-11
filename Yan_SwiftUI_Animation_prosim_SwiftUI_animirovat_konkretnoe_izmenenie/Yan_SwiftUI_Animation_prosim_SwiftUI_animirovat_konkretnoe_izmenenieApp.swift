866d88b60ecc6c204f7355d6720fa61805ee2556
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 10:11:52 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animation_prosim_SwiftUI_animirovat_konkretnoe_izmenenie/Yan_SwiftUI_Animation_prosim_SwiftUI_animirovat_konkretnoe_izmenenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Animation_prosim_SwiftUI_animirovat_konkretnoe_izmenenieApp.swift
+//  Yan_SwiftUI_Animation_prosim_SwiftUI_animirovat_konkretnoe_izmenenie
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Animation_prosim_SwiftUI_animirovat_konkretnoe_izmenenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
